e85ba7e4757c27dbb4a3631acb90228dc60f017f
SiddheshTambe45 <siddhesh11darkhorse@gmail.com>
2024-10-14 23:08:07 +0530

    second commit

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,10 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        appDir: true, // Enable if using App Router
+        serverComponentsExternalPackages: ['axios'], // Include axios for server-side components
+      },
   images: {
       remotePatterns: [
           {
