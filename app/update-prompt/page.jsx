bb59e28cc697d7336d8fccc90e393a05e2d09f0e
SiddheshTambe45 <siddhesh11darkhorse@gmail.com>
2024-10-21 12:33:08 +0530

    js mastery code

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,70 +1,137 @@
-'use client';
+// 'use client';
 
-import {Suspense, useEffect, useState} from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
-import Form from '@components/Form';
-import axios from 'axios';
+// import {Suspense, useEffect, useState} from 'react';
+// import { useRouter, useSearchParams } from 'next/navigation';
+// import Form from '@components/Form';
+// import axios from 'axios';
 
-// Force the page to use dynamic rendering (client-side only)
-export const dynamic = 'force-dynamic';
+// // Force the page to use dynamic rendering (client-side only)
+// export const dynamic = 'force-dynamic';
+
+// const UpdatePrompt = () => {
+
+//     const router = useRouter();
+//     const searchParams = useSearchParams();
+//     const promptId = searchParams?.get('id');
+
+//     const [submitting, setSubmitting] = useState(false);
+//     const [post, setPost] = useState({
+//         prompt: '',
+//         tag: ''
+//     })
+
+//     const updatePrompt = async(e) => {
+//       e.preventDefault();
+//       setSubmitting(true);
+
+//       if(!promptId){
+//         return alert('Prompt ID not found');
+//       }
+
+//       try {
+//         const response = await axios.patch(`/api/prompt/${promptId}`,{
+//           prompt: post.prompt,
+//           tag: post.tag
+//         });
+
+//         if(response.status === 200){
+//           router.push('/');
+//         }
+//       } catch (error) {
+//         console.log(error);
+//       } finally{
+//         setSubmitting(false);
+//       }
+//     }
+
+//     useEffect(()=>{
+//         const getPromptDetails = async() => {
+//             const response = await axios.get(`/api/prompt/${promptId}`);
+//             setPost({
+//                 prompt: response.data.prompt,
+//                 tag: response.data.tag
+//             })
+//         }
+
+//         if(promptId) getPromptDetails();
+//     },[promptId])
+
+//   return (
+//         <Form
+//         type="Edit"
+//         post={post}
+//         setPost={setPost}
+//         submitting={submitting}
+//         handleSubmit={updatePrompt}
+//       />
+//   )
+// }
+
+// export default UpdatePrompt;
+
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import Form from "@components/Form";
 
 const UpdatePrompt = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const promptId = searchParams.get("id");
 
-    const router = useRouter();
-    const searchParams = useSearchParams();
-    const promptId = searchParams?.get('id');
+  const [post, setPost] = useState({ prompt: "", tag: "", });
+  const [submitting, setIsSubmitting] = useState(false);
 
-    const [submitting, setSubmitting] = useState(false);
-    const [post, setPost] = useState({
-        prompt: '',
-        tag: ''
-    })
+  useEffect(() => {
+    const getPromptDetails = async () => {
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
 
-    const updatePrompt = async(e) => {
-      e.preventDefault();
-      setSubmitting(true);
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
+    };
 
-      if(!promptId){
-        return alert('Prompt ID not found');
-      }
+    if (promptId) getPromptDetails();
+  }, [promptId]);
 
-      try {
-        const response = await axios.patch(`/api/prompt/${promptId}`,{
+  const updatePrompt = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    if (!promptId) return alert("Missing PromptId!");
+
+    try {
+      const response = await fetch(`/api/prompt/${promptId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
           prompt: post.prompt,
-          tag: post.tag
-        });
+          tag: post.tag,
+        }),
+      });
 
-        if(response.status === 200){
-          router.push('/');
-        }
-      } catch (error) {
-        console.log(error);
-      } finally{
-        setSubmitting(false);
+      if (response.ok) {
+        router.push("/");
       }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
     }
-
-    useEffect(()=>{
-        const getPromptDetails = async() => {
-            const response = await axios.get(`/api/prompt/${promptId}`);
-            setPost({
-                prompt: response.data.prompt,
-                tag: response.data.tag
-            })
-        }
-
-        if(promptId) getPromptDetails();
-    },[promptId])
+  };
 
   return (
-        <Form
-        type="Edit"
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={updatePrompt}
-      />
-  )
-}
+    <Form
+      type='Edit'
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={updatePrompt}
+    />
+  );
+};
 
 export default UpdatePrompt;
